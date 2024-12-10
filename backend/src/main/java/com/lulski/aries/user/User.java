ba03235ecf48a9c8f38e8ef9c5906f02d0503725
Ichsan Siswoputranto <milunes@gmail.com>
2024-12-10 14:30:17 +1100

    update User.java add id field so UserRepository.save() will update matching document instead of creating new one

--- backend/src/main/java/com/lulski/aries/user/User.java
+++ backend/src/main/java/com/lulski/aries/user/User.java
@@ -1,21 +1,31 @@
 package com.lulski.aries.user;
 
-import org.springframework.data.annotation.Id;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("users")
 public class User {
 
+    private ObjectId id;
     private String username;
     private String firstName;
     private String lastName;
     private String email;
 
-    public User(String username, String firstName, String lastName, String email) {
+    public User(ObjectId id, String username, String firstName, String lastName, String email) {
+        this.id = id;
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
     }
 
     public String getUsername() {
