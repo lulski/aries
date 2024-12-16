b9d006738dcae93d15d1e96ab7a41add055b4671
Ichsan Siswoputranto <milunes@gmail.com>
2024-12-16 17:30:13 +1100

    housekeeping

--- backend/readme.md
+++ backend/readme.md
@@ -1,10 +1,6 @@
 # Project Aries
 
-play with reactive programming model.
-
-- springboot, backend
-- mongodb, db
-- react, frontend
+A project focused on exploring the reactive programming model using Spring Boot, MongoDB, and React.
 
 ---
 
--- backend/src/test/java/com/lulski/aries/config/TestMongoConfig.java
+++ backend/src/test/java/com/lulski/aries/config/TestMongoConfig.java
@@ -1,11 +1,16 @@
 package com.lulski.aries.config;
 
+import com.lulski.aries.user.UserRepository;
+import com.lulski.aries.user.UserService;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
 public class TestMongoConfig extends AbstractReactiveMongoConfiguration {
