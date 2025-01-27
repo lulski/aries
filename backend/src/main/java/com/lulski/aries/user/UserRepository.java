784fc45f1e4dd26d828fdd09f81617a5bc17b630
Ichsan Siswoputranto <milunes@gmail.com>
2025-01-27 14:51:54 +1100

    UserRepository update, annotate with @Repository

--- backend/src/main/java/com/lulski/aries/user/UserRepository.java
+++ backend/src/main/java/com/lulski/aries/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.lulski.aries.user;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,6 +12,7 @@ import reactor.core.publisher.Mono;
  * "https://docs.spring.io/spring-data/mongodb/reference/repositories/query-methods-details.html">mongo
  * repo doc</a>
  */
+@Repository
 public interface UserRepository extends ReactiveMongoRepository<User, ObjectId> {
     Flux<User> findAllByLastName(String value);
 
