8a318c37b7b7e435082c41584188f7aa41604a72
Ichsan Siswoputranto <milunes@gmail.com>
2025-02-10 13:59:22 +1100

    rubbish IDE formatter changes

--- backend/src/main/java/com/lulski/aries/post/PostService.java
+++ backend/src/main/java/com/lulski/aries/post/PostService.java
@@ -6,35 +6,34 @@ import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
 
-/**
- * Service for Post
- */
+/** Service for Post */
 @Service
 public class PostService {
-    private static final Logger logger = LoggerFactory.getLogger(PostService.class);
-    private PostRepository postRepository;
+  private static final Logger logger = LoggerFactory.getLogger(PostService.class);
+  private final PostRepository postRepository;
 
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+  public PostService(PostRepository postRepository) {
+    this.postRepository = postRepository;
+  }
 
-    /**
-     * Will do repository lookup for the supplied post then set isArchived field to
-     * true
-     *
-     * @param post
-     * @return
-     */
-    public Mono<Post> archivePost(Post post) {
-        return postRepository.findById(post.getId())
-                .doOnNext(p -> System.out.println(">>> found post with id : " + p.getId()))
-                .switchIfEmpty(Mono.error(new RuntimeException("No post with Id: " + post.getId() + " is found")))
-                .flatMap(found -> {
-                    found.setIsArchived(true);
-                    return postRepository.save(found);
-                })
-                .doOnSuccess(p -> logger.info("post Id: " + p.getId() + " is archived"))
-                .doOnError(err -> logger.error("unable to complete operation for p: " + err.getMessage()));
-    }
-
+  /**
+   * Will do repository lookup for the supplied post then set isArchived field to true
+   *
+   * @param post
+   * @return
+   */
+  public Mono<Post> archivePost(Post post) {
+    return postRepository
+        .findById(post.getId())
+        .doOnNext(p -> System.out.println(">>> found post with id : " + p.getId()))
+        .switchIfEmpty(
+            Mono.error(new RuntimeException("No post with Id: " + post.getId() + " is found")))
+        .flatMap(
+            found -> {
+              found.setIsArchived(true);
+              return postRepository.save(found);
+            })
+        .doOnSuccess(p -> logger.info("post Id: " + p.getId() + " is archived"))
+        .doOnError(err -> logger.error("unable to complete operation for p: " + err.getMessage()));
+  }
 }
--- backend/src/test/java/com/lulski/aries/config/TestWebSecurityConfig.java
+++ backend/src/test/java/com/lulski/aries/config/TestWebSecurityConfig.java
@@ -2,26 +2,25 @@ package com.lulski.aries.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-/**
- * Test config to disable Spring security
- */
+/** Test config to disable Spring security */
 @TestConfiguration
+@Profile("test")
 public class TestWebSecurityConfig {
 
-    /**
-     * override SpringSecurity SecurityWebFilterChain bean
-     */
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-        httpSecurity
-                .csrf(csrfSpec -> csrfSpec.disable())
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-                        .anyExchange()
-                        .permitAll());
+  /** override SpringSecurity SecurityWebFilterChain bean */
+  @Bean
+  @Primary
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+    httpSecurity
+        .csrf(csrfSpec -> csrfSpec.disable())
+        .authorizeExchange(
+            authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll());
 
-        return httpSecurity.build();
-    }
+    return httpSecurity.build();
+  }
 }
