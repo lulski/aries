f4691dee1107ca70d56f895055ad18ecc6b94215
Ichsan Siswoputranto <milunes@gmail.com>
2025-02-17 16:01:11 +1100

    Update TestcontainerMongoConfig.java
    formatter change

--- backend/src/test/java/com/lulski/aries/config/TestcontainerMongoConfig.java
+++ backend/src/test/java/com/lulski/aries/config/TestcontainerMongoConfig.java
@@ -1,35 +1,33 @@
 package com.lulski.aries.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
-/**
- * Testcontainer config
- */
+/** Testcontainer config */
 @Configuration
 @Profile("testcontainer")
 public class TestcontainerMongoConfig extends AbstractReactiveMongoConfiguration {
 
-    public TestcontainerMongoConfig() {
-        MongoDbContainerUtil.getMongoDbContainer().start();
-    }
+  public TestcontainerMongoConfig() {
+    MongoDbContainerUtil.getMongoDbContainer().start();
+  }
 
-    @Override
-    protected String getDatabaseName() {
-        return "aries_testcontainer";
-    }
+  @Override
+  protected String getDatabaseName() {
+    return "aries_testcontainer";
+  }
 
-    @Override
-    public MongoClient reactiveMongoClient() {
-        ConnectionString connectionString = new ConnectionString(MongoDbContainerUtil.getMongoDbContainer()
-                .getConnectionString());
-        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
-        return MongoClients.create(settings);
-    }
+  @Override
+  public MongoClient reactiveMongoClient() {
+    ConnectionString connectionString =
+        new ConnectionString(MongoDbContainerUtil.getMongoDbContainer().getConnectionString());
+    MongoClientSettings settings =
+        MongoClientSettings.builder().applyConnectionString(connectionString).build();
+    return MongoClients.create(settings);
+  }
 }
