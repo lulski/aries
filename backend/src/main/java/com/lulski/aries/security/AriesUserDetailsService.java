df552cb9b4c02ca600b5e948e8a174a602529898
Ichsan Siswoputranto <milunes@gmail.com>
2025-02-17 15:01:25 +1100

    SpringSecurity UserDetailsService now fetches users from the database

---
+++ backend/src/main/java/com/lulski/aries/security/AriesUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.lulski.aries.security;
+
+import com.lulski.aries.user.User;
+import com.lulski.aries.user.UserRepository;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AriesUserDetailsService implements ReactiveUserDetailsService {
+
+  private UserRepository userRepository;
+
+  public AriesUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public Mono<UserDetails> findByUsername(String username) {
+    return userRepository
+        .findTopByUsername(username)
+        .map(
+            user -> {
+              User.UserBuilder userBuilder = new User.UserBuilder();
+
+              userBuilder
+                  .password(user.getPassword())
+                  .email(user.getEmail())
+                  .username(user.getUsername())
+                  .firstname(user.getFirstName())
+                  .lastname(user.getLastName())
+                  .authorities(user.getAuthoritiesNames());
+
+              return userBuilder.build();
+            });
+  }
+}
