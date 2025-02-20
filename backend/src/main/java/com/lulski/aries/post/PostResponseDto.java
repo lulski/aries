90a7375a809632f0b75ce961f01b8dd701d8ae38
Ichsan Siswoputranto <milunes@gmail.com>
2025-02-20 15:09:11 +1100

    update Post domain
    - add insertNew service and test

---
+++ backend/src/main/java/com/lulski/aries/post/PostResponseDto.java
@@ -0,0 +1,3 @@
+package com.lulski.aries.post;
+
+public record PostResponseDto(int code, String content, String author, String createdDateTime) {}
--- backend/src/test/java/com/lulski/aries/config/TestWebSecurityConfig.java
+++ backend/src/test/java/com/lulski/aries/config/TestWebSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /** Test config to disable Spring security */
@@ -22,5 +27,18 @@ public class TestWebSecurityConfig {
             authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll());
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+    UserDetails userDetails =
+        User.withUsername("test").password(passwordEncoder.encode("test")).roles("USER").build();
+
+    return new InMemoryUserDetailsManager(userDetails);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 }
