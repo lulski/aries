f33b42c447cae9fdf2d7dddf11e2f61a83a6cb2e
Ichsan Siswoputranto <milunes@gmail.com>
2025-03-05 15:20:54 +1100

    update unittest

--- backend/src/test/java/com/lulski/aries/post/PostHandlerTest.java
+++ backend/src/test/java/com/lulski/aries/post/PostHandlerTest.java
@@ -1,6 +1,8 @@
 package com.lulski.aries.post;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockUser;
 
 import com.lulski.aries.config.TestWebSecurityConfig;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
@@ -26,6 +29,8 @@ class PostHandlerTest {
   @Autowired private WebTestClient webTestClient;
 
   @Autowired private ApplicationContext applicationContext;
+
+  @MockitoBean private PostService postService;
 
   @Test
   void contextLoad() {
@@ -54,5 +59,31 @@ class PostHandlerTest {
         .exchange()
         .expectStatus()
         .is2xxSuccessful();
+  }
+
+  @Test
+  void testErrorHandler() {
+
+    when(postService.insertNew(any(), any())).thenThrow(new RuntimeException("error happened"));
+
+    PostRequestDto postRequestDto = new PostRequestDto("is this it", "modern age");
+
+    User mockUser =
+        new User.UserBuilder()
+            .authorities(Set.of("USER"))
+            .firstname("kocu")
+            .lastname("gonzales")
+            .password("Test")
+            .build();
+
+    webTestClient
+        //        .mutateWith(mockUser("test").password("test").authorities("USER"))
+        .mutateWith(mockUser(mockUser))
+        .post()
+        .uri("/posts")
+        .body(BodyInserters.fromValue(postRequestDto))
+        .exchange()
+        .expectStatus()
+        .is5xxServerError();
   }
 }
