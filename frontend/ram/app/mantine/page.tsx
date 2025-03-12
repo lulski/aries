62f2d65fb297e18478702ec8f0e79a91462919d9
Ichsan Siswoputranto <milunes@gmail.com>
2025-03-12 22:24:16 +1100

    frontend add mantine

---
+++ frontend/ram/app/mantine/page.tsx
@@ -0,0 +1,23 @@
+import { Container } from '@mantine/core';
+
+export default function Demo() {
+  const demoProps = {
+    bg: 'var(--mantine-color-red-light)',
+    h: 50,
+    mt: 'md',
+  };
+
+  return (
+    <>
+      <Container {...demoProps}>Default Container</Container>
+
+      <Container size="xs" {...demoProps}>
+        xs Container
+      </Container>
+
+      <Container px={0} size={480} {...demoProps}>
+        480px Container without padding
+      </Container>
+    </>
+  );
+}
