b9d5a14e2ac4cbe8ff6d439c4c9c7ab755d981e6
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-11 16:08:07 +1000

    frontend - set session cookie post authentication
    - install iron-session
    - update api/authenticate to store session post successful authentication
    - add api/me route to fetch the stored session
    - update lib/definition.ts with SessionData
    - update env.d.ts with SESSION_KEY & COOKIE_NAME

--- frontend/ram/env.d.ts
+++ frontend/ram/env.d.ts
@@ -3,6 +3,8 @@ declare namespace NodeJS {
     interface ProcessEnv {
       API_AUTH_URL: string;
       API_POST_URL: string;
+      SESSION_KEY: string;
+      COOKIE_NAME: string;
       // Add more vars here as needed
     }
   }
--- frontend/ram/src/app/api/authenticate/route.ts
+++ frontend/ram/src/app/api/authenticate/route.ts
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 import { LoginFormSchema, LoginFormState } from "@/app/lib/definitions";
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import { log } from "console";
+import { SessionData } from "@/app/lib/definitions";
 
 export async function POST(request: Request) {
   const API_URL = process.env.API_AUTH_URL;
   const USERNAME = process.env.API_USERNAME;
   const PASSWORD = process.env.API_PASSWORD;
+  const SESSION_KEY = process.env.SESSION_KEY;
+  const COOKIE_NAME = process.env.COOKIE_NAME;
 
   
   const body = await request.json();
 
   const validatedField = LoginFormSchema.safeParse(body);
+
 
   if (!validatedField.success) {
     const issues = validatedField.error.errors;
@@ -39,7 +46,14 @@ export async function POST(request: Request) {
       { status: 401 }
     );
   }
+  
 
   const data = await response.json();
+  const session = await getIronSession<SessionData>(await cookies(), {password: SESSION_KEY, cookieName: COOKIE_NAME});
+  session.username = data.username;
+  session.firstname = data.firstName;
+  session.lastname = data.lastName;
+  session.authorities = data.roles;
+  await session.save();  
   return NextResponse.json({ success: true, data });
 }
