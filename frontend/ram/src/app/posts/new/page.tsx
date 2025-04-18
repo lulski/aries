54e0512cf7f15767db83dd2d980702ec85cc815b
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-18 19:34:23 +1000

    Update page.tsx

--- frontend/ram/src/app/posts/new/page.tsx
+++ frontend/ram/src/app/posts/new/page.tsx
@@ -1,10 +1,31 @@
 import AuthGuard from "@/app/components/AuthGuard";
+import { SessionProvider } from "@/app/context/SessionContext";
+import { SessionData } from "@/app/lib/definitions";
+import { getSessionData } from "@/app/lib/sessionUtil";
+import { redirect } from "next/navigation";
 
-export default function NewPostPage() {
+export default async function NewPostPage() {
+  console.log(">>> fetching cookie");
+  const session = await getSessionData();
+
   return (
     <AuthGuard>
-      <h1>Create a New Post</h1>
-      {/* You can add your form here */}
+      <h1>Create a New Post {session.firstname} !</h1>
     </AuthGuard>
   );
+
+
+
+
+  // const session: SessionData = await getSessionData();
+
+  // if (!session?.isAuthenticated) {
+  //   redirect("/login"); // SSR-based redirect
+  // }
+
+  // return (
+  //   // <SessionProvider value={session}>
+  //     <h1>Create a New Post, {session.firstname}!</h1>
+  //   // </SessionProvider>
+  // ); 
 }
