047bc2c6dbffbd5fd09ec82c1cc655e16eb3591e
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-18 15:50:44 +1000

    Update AuthGuard.tsx
    - attempt to set SessionData in AuthGuard component

--- frontend/ram/src/app/components/AuthGuard.tsx
+++ frontend/ram/src/app/components/AuthGuard.tsx
@@ -4,20 +4,28 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Loader, Center } from "@mantine/core";
+import { SessionData } from "../lib/definitions";
 
 type Props = {
-  children: React.ReactNode;
+  children: (session: SessionData) => React.ReactNode;
 };
 
 export default function AuthGuard({ children }: Props) {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const [session, setSession] = useState<SessionData | null>(null);
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
         const res = await fetch("/api/me");
+
         if (!res.ok) throw new Error("Not authenticated");
+
+        const data = await res.json();
+        console.log("Session data:", data.session);
+        setSession(data.session);
+
         setLoading(false);
       } catch (err) {
         router.replace("/login");
@@ -34,6 +42,8 @@ export default function AuthGuard({ children }: Props) {
       </Center>
     );
   }
+
+  if (!session?.isAuthenticated) return null;
 
   return <>{children}</>;
 }
