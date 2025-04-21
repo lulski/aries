a868b8f224ced5580ab3cd8be6916627e7a5fdc9
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-21 13:14:36 +1000

    editor tweak

--- frontend/ram/src/app/components/AuthGuard.tsx
+++ frontend/ram/src/app/components/AuthGuard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState, createContext, useContext } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import { Loader, Center } from "@mantine/core";
 import { SessionData } from "@/app/lib/definitions";
 import { SessionProvider } from "../context/SessionContext";
@@ -10,35 +10,34 @@ type Props = {
   children: React.ReactNode;
 };
 
-
-export default function AuthGuard({ children }: Props ) {
+export default function AuthGuard({ children }: Props) {
   const [session, setSession] = useState<SessionData | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
-    
     const checkAuth = async () => {
       try {
         const res = await fetch("/api/me");
 
-        //fail api call should trigger an error 
         if (!res.ok) throw new Error("Not authenticated");
 
-        const data:SessionData = await res.json();
+        const data: SessionData = await res.json();
 
         console.log("Session data:", data);
-              
+        
         setSession(data);
         setLoading(false);
-                
       } catch (err) {
-        router.replace("/login");
+        // Encode the current path to handle special characters
+        const returnUrl = encodeURIComponent(pathname);
+        router.replace(`/login?returnUrl=${returnUrl}`);
       }
     };
     
     checkAuth();
-  }, [router]);
+  }, [router, pathname]);
 
   if (loading || !session) {
     return (
@@ -48,7 +47,9 @@ export default function AuthGuard({ children }: Props ) {
     );
   }
 
-  return (<SessionProvider value={session}>
-    {children}
-  </SessionProvider>);
+  return (
+    <SessionProvider value={session}>
+      {children}
+    </SessionProvider>
+  );
 }
