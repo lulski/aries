496703a830793471280899adf2306952a87626fe
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-24 15:36:29 +1000

    Update Post.tsx
    - make card click to navigate to view post page

--- frontend/ram/src/app/components/Post.tsx
+++ frontend/ram/src/app/components/Post.tsx
@@ -7,30 +7,40 @@ import {
   Title,
   CardSection,
   Divider,
+  NavLink,
 } from "@mantine/core";
 import { PostData } from "../lib/definitions";
+import sanitizeHtml from 'sanitize-html';
+import Link from "next/link";
 
 export default function Post(post: PostData) {
+  
+  const sanitizedContent = sanitizeHtml(post.content, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
+  
   return (
     <>
-      <Card w={400} shadow="sm" padding="lg" radius="md" withBorder>
+
+    <Card shadow="sm" padding="lg" radius="md" withBorder component={Link} href={`/posts/${post.id}`}>
         <CardSection>
-          <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png" />
+          {/* <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png" /> */}
         </CardSection>
 
         <Group justify="space-between" mt="md" mb="xs">
           <Title order={5}>{post.title}</Title>
         </Group>
 
-        <Text size="sm" c="dimmed">
-          {post.content}
-        </Text>
+        <Box size="sm"  dangerouslySetInnerHTML={{__html: sanitizedContent}} />
         <br />
         <Text size="xs" c="dark" ta="right">
           {post.author}
         </Text>
       </Card>
       <Divider my="md" />
+
+      
     </>
   );
 }
