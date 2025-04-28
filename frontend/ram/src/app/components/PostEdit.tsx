4ddea9a96f78d8407c13c2e21c97632e2f541038
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-28 17:00:22 +1000

    Update PostEdit.tsx
    - onChange -> onChangeAction to conform to Next.js expectation

--- frontend/ram/src/app/components/PostEdit.tsx
+++ frontend/ram/src/app/components/PostEdit.tsx
@@ -13,16 +13,17 @@ import SubScript from "@tiptap/extension-subscript";
 import { InputWrapper, Text } from "@mantine/core";
 import { error } from "console";
 
-
 interface PostEditProps {
-  
   content: string;
-  onChange: (content: string) => void;
-  error?: string|null;
+  onChangeAction: (content: string) => void;
+  error?: string | null;
 }
 
-
-export default function PostEdit({ content, onChange, error }: PostEditProps) {
+export default function PostEdit({
+  content,
+  onChangeAction,
+  error,
+}: PostEditProps) {
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -34,70 +35,71 @@ export default function PostEdit({ content, onChange, error }: PostEditProps) {
       TextAlign.configure({ types: ["heading", "paragraph"] }),
     ],
     content,
-    onUpdate: ({ editor }) => onChange(editor.getHTML()),
+    onUpdate: ({ editor }) => onChangeAction(editor.getHTML()),
     immediatelyRender: false,
   });
 
   return (
     <>
+      <InputWrapper
+        label="Content:"
+        description=""
+        id="post-editor-wrapper"
+        error={error}
+      >
+        <RichTextEditor
+          id="post-editor"
+          editor={editor}
+          styles={{ content: { minHeight: 400 } }}
+        >
+          <RichTextEditor.Toolbar sticky stickyOffset={60}>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Bold />
+              <RichTextEditor.Italic />
+              <RichTextEditor.Underline />
+              <RichTextEditor.Strikethrough />
+              <RichTextEditor.ClearFormatting />
+              <RichTextEditor.Highlight />
+              <RichTextEditor.Code />
+            </RichTextEditor.ControlsGroup>
 
-    <InputWrapper
-      label="Content:"
-      description=""
-      id="post-editor-wrapper"
-      error={error}
-    >
-    <RichTextEditor id="post-editor" editor={editor} styles={{content: {minHeight: 400}}}>
-      <RichTextEditor.Toolbar sticky stickyOffset={60}>
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Bold />
-          <RichTextEditor.Italic />
-          <RichTextEditor.Underline />
-          <RichTextEditor.Strikethrough />
-          <RichTextEditor.ClearFormatting />
-          <RichTextEditor.Highlight />
-          <RichTextEditor.Code />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.H1 />
+              <RichTextEditor.H2 />
+              <RichTextEditor.H3 />
+              <RichTextEditor.H4 />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.H1 />
-          <RichTextEditor.H2 />
-          <RichTextEditor.H3 />
-          <RichTextEditor.H4 />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Blockquote />
+              <RichTextEditor.Hr />
+              <RichTextEditor.BulletList />
+              <RichTextEditor.OrderedList />
+              <RichTextEditor.Subscript />
+              <RichTextEditor.Superscript />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Blockquote />
-          <RichTextEditor.Hr />
-          <RichTextEditor.BulletList />
-          <RichTextEditor.OrderedList />
-          <RichTextEditor.Subscript />
-          <RichTextEditor.Superscript />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Link />
+              <RichTextEditor.Unlink />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Link />
-          <RichTextEditor.Unlink />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.AlignLeft />
+              <RichTextEditor.AlignCenter />
+              <RichTextEditor.AlignJustify />
+              <RichTextEditor.AlignRight />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.AlignLeft />
-          <RichTextEditor.AlignCenter />
-          <RichTextEditor.AlignJustify />
-          <RichTextEditor.AlignRight />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Undo />
+              <RichTextEditor.Redo />
+            </RichTextEditor.ControlsGroup>
+          </RichTextEditor.Toolbar>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Undo />
-          <RichTextEditor.Redo />
-        </RichTextEditor.ControlsGroup>
-      </RichTextEditor.Toolbar>
-
-      <RichTextEditor.Content />
-    </RichTextEditor>
-
-    </InputWrapper>
+          <RichTextEditor.Content />
+        </RichTextEditor>
+      </InputWrapper>
     </>
-    
   );
 }
