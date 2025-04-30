94dc9f08606f1c03d5bfdf6bc45527949995a346
Ichsan Siswoputranto <milunes@gmail.com>
2025-04-30 19:22:01 +1000

    Update Post.tsx
    - remove hardcoded c to accomodate dark|light theme
    - remove use Divider, use styles instead

--- frontend/ram/src/app/components/Post.tsx
+++ frontend/ram/src/app/components/Post.tsx
@@ -10,20 +10,26 @@ import {
   NavLink,
 } from "@mantine/core";
 import { PostData } from "../lib/definitions";
-import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from "sanitize-html";
 import Link from "next/link";
 
 export default function Post(post: PostData) {
-  
   const sanitizedContent = sanitizeHtml(post.content, {
     allowedTags: [],
     allowedAttributes: {},
   });
-  
+
   return (
     <>
-
-    <Card shadow="sm" padding="lg" radius="md" withBorder component={Link} href={`/posts/${post.id}`}>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+        component={Link}
+        href={`/posts/${post.id}`}
+        styles={{ root: { marginBottom: 10 } }} // Adds 10px to the bottom
+      >
         <CardSection>
           {/* <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png" /> */}
         </CardSection>
@@ -32,15 +38,12 @@ export default function Post(post: PostData) {
           <Title order={5}>{post.title}</Title>
         </Group>
 
-        <Box size="sm"  dangerouslySetInnerHTML={{__html: sanitizedContent}} />
+        <Box size="sm" dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
         <br />
-        <Text size="xs" c="dark" ta="right">
+        <Text size="xs" ta="right">
           {post.author}
         </Text>
       </Card>
-      <Divider my="md" />
-
-      
     </>
   );
 }
