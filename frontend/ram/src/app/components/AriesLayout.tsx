e3453f22cc301e8a88389eee86b7b85375af8dcb
Ichsan Siswoputranto <milunes@gmail.com>
2025-05-12 11:11:52 +1000

    Move ThemeProvider to layout.tsx

--- frontend/ram/src/app/components/AriesLayout.tsx
+++ frontend/ram/src/app/components/AriesLayout.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { Anchor, AppShell, Burger, Group, Stack, Title } from "@mantine/core";
+import {
+  Anchor,
+  AppShell,
+  Burger,
+  Group,
+  Stack,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { ReactNode } from "react";
 import { AriesLayoutProps } from "@/app/types/AriesLayoutProperties";
 import { ThemeToggleButton } from "./ThemeToggleButton/ThemeToggleButton";
-import { ThemeProvider } from "../context/ThemeContext";
+import { useTheme } from "../context/ThemeContext";
 
 export default function AriesLayout({ children }: { children: ReactNode }) {
   const [navbarIsOpen, { toggle }] = useDisclosure();
+  const theme = useTheme();
 
   const layoutProps: AriesLayoutProps = {
     navbarItems: [
@@ -20,57 +29,70 @@ export default function AriesLayout({ children }: { children: ReactNode }) {
   };
 
   return (
-    <ThemeProvider>
-      <AppShell
-        suppressHydrationWarning
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: "sm",
-          collapsed: { mobile: !navbarIsOpen },
-        }}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Group h="100%" px="md" justify="space-between">
-            <Burger
-              opened={navbarIsOpen}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="sm"
-            />
-            <Group>
-              <Stack gap={0}>
-                <Title order={5} hiddenFrom="xs" suppressHydrationWarning>
-                  Coco Classico
-                </Title>
-                <Title order={3} visibleFrom="xs" hiddenFrom="sm">
-                  Coco Classico
-                </Title>
-                <Title order={2} visibleFrom="sm" hiddenFrom="md">
-                  Coco Classico
-                </Title>
-                <Title order={1} visibleFrom="md">
-                  Coco Classico
-                </Title>
-              </Stack>
-            </Group>
-            <Group>
-              <ThemeToggleButton />
-            </Group>
+    <AppShell
+      suppressHydrationWarning
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !navbarIsOpen },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md" justify="space-between">
+          <Burger
+            opened={navbarIsOpen}
+            onClick={toggle}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Group>
+            <Stack gap={0}>
+              <Title
+                order={5}
+                hiddenFrom="xs"
+                suppressHydrationWarning
+                c={theme.primaryColor}
+              >
+                Coco Classico
+              </Title>
+              <Title
+                order={3}
+                visibleFrom="xs"
+                hiddenFrom="sm"
+                c={theme.primaryColor}
+              >
+                Coco Classico
+              </Title>
+              <Title
+                order={2}
+                visibleFrom="sm"
+                hiddenFrom="md"
+                c={theme.primaryColor}
+              >
+                Coco Classico
+              </Title>
+              <Title order={1} visibleFrom="md" c={theme.primaryColor}>
+                Coco Classico
+              </Title>
+            </Stack>
           </Group>
-        </AppShell.Header>
+          <Group>
+            <ThemeToggleButton />
+          </Group>
+        </Group>
+      </AppShell.Header>
 
-        <AppShell.Navbar p="md">
-          {layoutProps.navbarItems?.map((item, index) => (
-            <Anchor size="md" key={index} href={item.href}>
-              {item.label}
-            </Anchor>
-          ))}
-        </AppShell.Navbar>
+      <AppShell.Navbar p="md">
+        {layoutProps.navbarItems?.map((item, index) => (
+          <Anchor size="md" key={index} href={item.href}>
+            {item.label}
+          </Anchor>
+        ))}
+      </AppShell.Navbar>
 
-        <AppShell.Main>{children}</AppShell.Main>
-      </AppShell>
-    </ThemeProvider>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
   );
 }
