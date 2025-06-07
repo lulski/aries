9453b3b7631e86ab82c353b8adc5bd8ebac7736d
Ichsan Siswoputranto <milunes@gmail.com>
2025-06-07 14:50:08 +1000

    frontend refactor post/ to use post.title instead of post.id

--- frontend/ram/src/app/posts/[param]/page.tsx
+++ frontend/ram/src/app/posts/[param]/page.tsx
@@ -1,17 +1,27 @@
 import Post from "@/app/components/Post";
 import { PostData, SessionData } from "@/app/lib/definitions";
-import { fetchPost } from "@/app/lib/fetchPost";
+import { fetchPostById, fetchPostByTitle } from "@/app/lib/fetchPost";
 import { getSessionData } from "@/app/lib/sessionUtil";
 import { Button, Group } from "@mantine/core";
 import link from "next/link";
 
-export default async function getPostById({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const response = await fetchPost(id);
+type Params = {
+  param: string;
+};
+
+export default async function viewPost({ params }: { params: Params }) {
+  const { param } = params;
+
+  const isNumeric = /^\d+$/.test(param);
+
+  let response;
+  if (isNumeric) {
+    // fetch by numeric ID
+    response = await fetchPostById(param);
+  } else {
+    // fetch by slug
+    response = await fetchPostByTitle(param);
+  }
   const post: PostData = response.data;
   const sessionData: SessionData = await getSessionData();
 
@@ -20,7 +30,7 @@ export default async function getPostById({
       <Group justify="flex-end" mb={"10px"}>
         {sessionData.isAuthenticated &&
           sessionData.authorities.includes("ADMIN") && (
-            <Button component={link} href={`/posts/${id}/edit`}>
+            <Button component={link} href={`/posts/${param}/edit`}>
               Edit Post
             </Button>
           )}
