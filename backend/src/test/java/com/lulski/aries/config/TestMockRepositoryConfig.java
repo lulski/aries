a5686163ab3ad9a2eccc1f4bf943ee9f08bf47a1
Ichsan Siswoputranto <milunes@gmail.com>
2025-06-14 14:46:41 +1000

    IDE formatter change

--- backend/src/test/java/com/lulski/aries/config/TestMockRepositoryConfig.java
+++ backend/src/test/java/com/lulski/aries/config/TestMockRepositoryConfig.java
@@ -27,29 +27,29 @@ import reactor.core.publisher.Mono;
  */
 // @TestConfiguration
 @Component
-@EnableAutoConfiguration(exclude = { MongoReactiveRepositoriesAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = {MongoReactiveRepositoriesAutoConfiguration.class})
 @Profile("mock")
 public class TestMockRepositoryConfig {
 
     public static final String mockPostId = "1234b64f525959be00d07c0b";
 
     private static final User mockAuthor = new User.UserBuilder()
-            .username("dummyUser")
-            .password("dummyPassword")
-            .authorities(Set.of("ADMIN", "USER"))
-            .firstname("bob")
-            .lastname("gonzales")
-            .email("bgz@www.com")
-            .build();
+        .username("dummyUser")
+        .password("dummyPassword")
+        .authorities(Set.of("ADMIN", "USER"))
+        .firstname("bob")
+        .lastname("gonzales")
+        .email("bgz@www.com")
+        .build();
     private static final Post mockPost = new Post(
-            new ObjectId(mockPostId),
-            "how to gitgood part two",
-            "you just have to grind 3 hours everyday",
-            mockAuthor.getUsername(),
-            LocalDateTime.now(),
-            LocalDateTime.now(),
-            false,
-            false);
+        new ObjectId(mockPostId),
+        "how to gitgood part two",
+        "you just have to grind 3 hours everyday",
+        mockAuthor.getUsername(),
+        LocalDateTime.now(),
+        LocalDateTime.now(),
+        false,
+        false);
 
     /**
      * mock of userRepository bean, enable or disable from
@@ -62,25 +62,25 @@ public class TestMockRepositoryConfig {
     public UserRepository userRepository() {
         UserRepository userRepository = mock(UserRepository.class);
         when(userRepository.save(any(User.class)))
-                .thenAnswer(
-                        invocation -> {
-                            User argument = invocation.getArgument(0);
-                            argument.setId(new ObjectId("67b2abe390f04d67290f4523"));
-                            return Mono.just(argument);
-                        });
+            .thenAnswer(
+                invocation -> {
+                    User argument = invocation.getArgument(0);
+                    argument.setId(new ObjectId("67b2abe390f04d67290f4523"));
+                    return Mono.just(argument);
+                });
 
         when(userRepository.findTopByUsername(anyString()))
-                .thenAnswer(
-                        inv -> {
-                            User user = new User(
-                                    mockAuthor.getId(),
-                                    inv.getArgument(0),
-                                    "dummyPassword",
-                                    mockAuthor.getFirstName(),
-                                    mockAuthor.getLastName(),
-                                    mockAuthor.getEmail());
-                            return Mono.just(user);
-                        });
+            .thenAnswer(
+                inv -> {
+                    User user = new User(
+                        mockAuthor.getId(),
+                        inv.getArgument(0),
+                        "dummyPassword",
+                        mockAuthor.getFirstName(),
+                        mockAuthor.getLastName(),
+                        mockAuthor.getEmail());
+                    return Mono.just(user);
+                });
         return userRepository;
     }
 
@@ -96,11 +96,11 @@ public class TestMockRepositoryConfig {
         PostRepository postRepository = mock(PostRepository.class);
 
         when(postRepository.save(any(Post.class)))
-                .thenAnswer(
-                        invocation -> {
-                            Post argument = invocation.getArgument(0);
-                            return Mono.just(argument);
-                        });
+            .thenAnswer(
+                invocation -> {
+                    Post argument = invocation.getArgument(0);
+                    return Mono.just(argument);
+                });
 
         when(postRepository.findById(any(ObjectId.class))).thenReturn(Mono.just(mockPost));
         // when(postRepository.findById(any(ObjectId.class))).thenReturn(Mono.error(new
@@ -120,7 +120,7 @@ public class TestMockRepositoryConfig {
 
         when(postRepository.save(any())).thenAnswer(invocationOnMock -> {
             Post post = invocationOnMock.getArgument(0);
-            if (post != null) {
+            if (post!=null) {
                 post.setId(new ObjectId());
                 return Mono.just(post);
             } else {
@@ -129,7 +129,7 @@ public class TestMockRepositoryConfig {
 
         });
 
-        when(postRepository.findByTitle(anyString())).thenAnswer(invocationOnMock -> {
+        when(postRepository.findTopByTitle(anyString())).thenAnswer(invocationOnMock -> {
             String title = invocationOnMock.getArgument(0);
             Post mockPost = new Post();
             mockPost.setTitle(title);
