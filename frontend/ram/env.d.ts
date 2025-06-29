edb4fb62673997bd5f75eb2c4fdd40f4cee702ce
Ichsan Siswoputranto <milunes@gmail.com>
2025-06-29 17:17:35 +1000

    Update ProcessEnv interface to include API_USERNAME and API_PASSWORD

--- frontend/ram/env.d.ts
+++ frontend/ram/env.d.ts
@@ -1,11 +1,12 @@
 // env.d.ts
 declare namespace NodeJS {
-    interface ProcessEnv {
-      API_AUTH_URL: string;
-      API_POST_URL: string;
-      SESSION_KEY: string;
-      COOKIE_NAME: string;
-      // Add more vars here as needed
-    }
+  interface ProcessEnv {
+    API_AUTH_URL: string;
+    API_POST_URL: string;
+    API_USERNAME: string;
+    API_PASSWORD: string;
+    SESSION_KEY: string;
+    COOKIE_NAME: string;
+    // Add more vars here as needed
   }
-  
+}
