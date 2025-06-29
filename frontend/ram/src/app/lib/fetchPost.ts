754d3e0469f6bee8d59a6bb027bf38c8a5930737
Ichsan Siswoputranto <milunes@gmail.com>
2025-06-29 17:17:27 +1000

    Refactor API to implement pagination for fetching posts and update response handling

--- frontend/ram/src/app/lib/fetchPost.ts
+++ frontend/ram/src/app/lib/fetchPost.ts
@@ -1,4 +1,26 @@
-export async function fetchPost(fetchPostUrl: string) {
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+  createdOn: string;
+  modifiefOn: string;
+}
+
+export interface PostApiResponse {
+  postDto: Post;
+  message: string;
+  page: number;
+  size: number;
+  total: number;
+}
+
+const API_POST_URL = process.env.API_POST_URL;
+const API_USERNAME = process.env.API_USERNAME;
+const API_PASSWORD = process.env.API_PASSWORD;
+
+export async function fetchPost(
+  fetchPostUrl: string
+): Promise<PostApiResponse> {
   const USERNAME = process.env.API_USERNAME;
   const PASSWORD = process.env.API_PASSWORD;
 
@@ -15,25 +37,40 @@ export async function fetchPost(fetchPostUrl: string) {
     }
 
     const data = await response.json();
-    return { success: true, data: data };
+    return data as PostApiResponse;
   } catch (error) {
     console.error("Error fetching post:", error);
-    return {
-      success: false,
-      error:
-        error instanceof Error ? error.message : "An unknown error occurred",
-    };
+    throw new Error("Failed to fetch post");
   }
 }
 
 export async function fetchPostById(id: string) {
-  const API_POST_URL = process.env.API_POST_URL;
   const fetchPostUrl = API_POST_URL + "?id=" + id;
   fetchPost(fetchPostUrl);
 }
 
 export async function fetchPostByTitle(title: string) {
-  const API_POST_URL = process.env.API_POST_URL;
   const fetchPostUrl = API_POST_URL + "?title=" + title;
   return fetchPost(fetchPostUrl);
+}
+
+export async function fetchPaginatedPosts(
+  currentPage: number,
+  size: number
+): Promise<PostApiResponse> {
+  const fetchUrl = `${API_POST_URL}?page=${currentPage}&size=${size}`;
+
+  const response = await fetch(fetchUrl, {
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(`${API_USERNAME}:${API_PASSWORD}`).toString("base64"),
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  return response.json();
 }
