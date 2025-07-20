cfbee2dddd0b5a455328c3022ece35ad6ea4a8aa
Ichsan Siswoputranto <milunes@gmail.com>
2025-07-20 16:44:48 +1000

    chore: update jest config and dev dependencies

--- frontend/ram/jest.setup.ts
+++ frontend/ram/jest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+//import "whatwg-fetch";
+
+// async function readNextResponseJson(response: Response): Promise<any> {
+//   const text = await response.text();
+//   return JSON.parse(text);
+// }
