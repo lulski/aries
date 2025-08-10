bb5bb4de102b27f927113d746dd4ed7c9e881e2d
Ichsan Siswoputranto <milunes@gmail.com>
2025-08-10 16:39:52 +1000

    chore: delete unused tf dir

---
+++ terraform-ec2-mongodb/modules/ec2-mongodb/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "mongodb" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  security_groups = [aws_security_group.mongodb_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y mongodb
+              systemctl start mongodb
+              systemctl enable mongodb
+              EOF
+
+  tags = {
+    Name = "MongoDB-Instance"
+  }
+}
+
+resource "aws_security_group" "mongodb_sg" {
+  name        = "mongodb_sg"
+  description = "Allow MongoDB access"
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/mongodb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "mongodb_public_ip" {
-  description = "Public IP of the MongoDB EC2 instance"
-  value       = aws_instance.mongodb.public_ip
-}
-
-output "mongodb_instance_id" {
-  description = "Instance ID of the MongoDB EC2 instance"
-  value       = aws_instance.mongodb.id
-}
