b02139117308283388d56a6f63244faa9e1300d9
Ichsan Siswoputranto <milunes@gmail.com>
2025-08-30 23:26:11 +1000

    feat: refactor how aries-jar is setup in ec2

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -9,6 +9,7 @@ provider "aws" {
   region = var.region
 }
 
+# provision s3bucket called aries-tf-state
 # this is to put terraform state in the cloud 
 resource "aws_s3_bucket" "aries-tf-state" {
   bucket = "aries-tf-state"
@@ -56,6 +57,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
+# configure this terraform context to put the state in the above s3 bucket
 terraform {
   backend "s3" {
     bucket = "aries-tf-state"
@@ -75,68 +77,3 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
   description = "The name of the DynamoDB table"
 }
-
-#### JAR start
-
-# #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket 
-# resource "aws_s3_bucket" "aries" {
-#   bucket = "aries-springboot-jar"
-# }
-
-# resource "aws_s3_object" "aries_backend_jar" {
-#   bucket = aws_s3_bucket.aries.id
-#   key    = "aries_jar"
-#   source = "../../backend/build/libs/aries-backend.jar"
-# }
-
-# # versioning the s3 bucket
-# #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
-# resource "aws_s3_bucket_versioning" "aries_versioning" {
-#   bucket = aws_s3_bucket.aries.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-# # encrypt all data written into the bucket, might be useful as it might store passwords etc
-# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-#   bucket = aws_s3_bucket.aries.id
-
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
-
-# # Explicitly allow all public access to the S3 bucket
-# resource "aws_s3_bucket_public_access_block" "disable_block" {
-#   bucket                  = aws_s3_bucket.aries.id
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
-
-# resource "aws_s3_bucket_policy" "public_read" {
-#   bucket = aws_s3_bucket.aries.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.aries.arn}/*"
-#       }
-#     ]
-#   })
-
-# }
-
-
-# output "aries_backend_jar_url" {
-#   description = "Public URL of the aries-backend.jar in S3 (if bucket/object is public)"
-#   value       = "https://${aws_s3_bucket.aries.bucket}.s3.${var.region}.amazonaws.com/${aws_s3_object.aries_backend_jar.key}"
-# }
