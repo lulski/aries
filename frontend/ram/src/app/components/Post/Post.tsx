16c39468ea7d337e1765dfce2755915ef3eba673
Ichsan Siswoputranto <milunes@gmail.com>
2025-11-16 22:22:35 +1100

    feat: frontend update to prepare for embedded image - Upgrade Tiptap 2 -> 3 - Refactor Post component, display content using Tiptap editor - Style  Title section with CSS

--- frontend/ram/src/app/components/Post/Post.tsx
+++ frontend/ram/src/app/components/Post/Post.tsx
@@ -1,5 +1,19 @@
-import { Box, Card, Group, Text, Title } from "@mantine/core";
-import sanitizeHtml from "sanitize-html";
+"use client";
+import {
+  Card,
+  createTheme,
+  Group,
+  MantineProvider,
+  Text,
+  Title,
+} from "@mantine/core";
+import { Link, RichTextEditor } from "@mantine/tiptap";
+import Highlight from "@tiptap/extension-highlight";
+import Superscript from "@tiptap/extension-superscript";
+import TextAlign from "@tiptap/extension-text-align";
+import { Color, TextStyle } from "@tiptap/extension-text-style";
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import { PostData } from "../../lib/definitions";
 
 type PostProps = {
@@ -7,37 +21,69 @@ type PostProps = {
   allowHtmlMarkup: boolean;
 };
 
-export default function Post({ post, allowHtmlMarkup }: PostProps) {
-  let displayContent;
-
-  const sanitizedContent = sanitizeHtml(post.content, {
-    allowedTags: [],
-    allowedAttributes: {
-      a: ["href", "target", "rel"],
+const theme = createTheme({
+  headings: {
+    textWrap: "wrap",
+  },
+  components: {
+    Title: {
+      styles: () => ({
+        root: {
+          textTransform: "capitalize",
+        },
+      }),
     },
+  },
+});
+
+export default function Post({ post, allowHtmlMarkup }: PostProps) {
+  // let displayContent;
+
+  // const sanitizedContent = sanitizeHtml(post.content, {
+  //   allowedTags: [],
+  //   allowedAttributes: {
+  //     a: ["href", "target", "rel"],
+  //   },
+  // });
+  // console.log("allowHtmlMarkup: " + allowHtmlMarkup);
+  // if (allowHtmlMarkup) {
+  //   displayContent = post.content;
+  // } else {
+  //   displayContent = sanitizedContent;
+  // }
+
+  const editor = useEditor({
+    shouldRerenderOnTransaction: true,
+
+    extensions: [
+      StarterKit,
+      TextStyle,
+      Color,
+      Link,
+      Superscript,
+      Highlight,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
+    ],
+    content: post.content,
+    immediatelyRender: false,
   });
-  console.log("allowHtmlMarkup: " + allowHtmlMarkup);
-  if (allowHtmlMarkup) {
-    displayContent = post.content;
-  } else {
-    displayContent = sanitizedContent;
-  }
 
   return (
-    <>
-      <Card
-        shadow="sm"
-        padding="lg"
-        radius="md"
-        withBorder
-        styles={{ root: { marginBottom: 10 } }}
-        id={post.id}
-      >
-        <Group justify="space-between" mt="md" mb="xs">
+    <MantineProvider theme={theme}>
+      <Card padding="lg" styles={{ root: { marginBottom: 10 } }} id={post.id}>
+        <Group justify="center" mb="md" align="right">
           <Title order={1}>{post.title}</Title>
         </Group>
 
-        <Box size="sm" dangerouslySetInnerHTML={{ __html: displayContent }} />
+        {/* <Box size="sm" dangerouslySetInnerHTML={{ __html: displayContent }} /> */}
+
+        <RichTextEditor
+          id="post"
+          editor={editor}
+          styles={{ content: { minHeight: 400 } }}
+        >
+          <RichTextEditor.Content />
+        </RichTextEditor>
         <br />
         <Text size="xs" ta="right">
           {post.author}
@@ -46,6 +92,6 @@ export default function Post({ post, allowHtmlMarkup }: PostProps) {
           {post.createdOn}
         </Text>
       </Card>
-    </>
+    </MantineProvider>
   );
 }
