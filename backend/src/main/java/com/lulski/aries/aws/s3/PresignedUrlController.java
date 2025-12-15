7723b66d5633bb4ffb4107ba2810c9ee6c1ea5d8
Ichsan Siswoputranto <milunes@gmail.com>
2025-12-15 15:34:35 +0700

    update presigned code

--- backend/src/main/java/com/lulski/aries/aws/s3/PresignedUrlController.java
+++ backend/src/main/java/com/lulski/aries/aws/s3/PresignedUrlController.java
@@ -1,28 +1,38 @@
 package com.lulski.aries.aws.s3;
 
-import java.time.Duration;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.util.Map;
+
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 @RestController
 public class PresignedUrlController {
 
     private final S3Presigner presigner;
 
+    private static final Logger logger = LoggerFactory.getLogger(PresignedUrlController.class);
+
     public PresignedUrlController(S3Presigner presigner) {
         this.presigner = presigner;
     }
 
+    /**
+     * generates URL that allows access to the supplied bucket object
+     */
     @GetMapping("/s3/presigned-url/{bucket}/{object}")
     public Mono<ResponseEntity<String>> generatePresignedUrl(
             @PathVariable String bucket,
@@ -43,6 +53,30 @@ public class PresignedUrlController {
 
             return ResponseEntity.ok(url);
         }).subscribeOn(Schedulers.boundedElastic());// move blocking work off main thread
+    }
+
+    /* Create a presigned URL to use in a subsequent PUT request */
+    public String createPresignedUrl(String bucketName, String keyName, Map<String, String> metadata) {
+        try (S3Presigner presigner = S3Presigner.create()) {
+
+            PutObjectRequest objectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .metadata(metadata)
+                    .build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10)) // The URL expires in 10 minutes.
+                    .putObjectRequest(objectRequest)
+                    .build();
+
+            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+            String myURL = presignedRequest.url().toString();
+            logger.info("Presigned URL to upload a file to: [{}]", myURL);
+            logger.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
+
+            return presignedRequest.url().toExternalForm();
+        }
     }
 
 }
---
+++ backend/src/test/java/com/lulski/aries/aws/s3/PresignedUrlControllerTest.java
@@ -0,0 +1,74 @@
+package com.lulski.aries.aws.s3;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import reactor.test.StepVerifier;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+class PresignedUrlControllerTest {
+
+    @Mock
+    private S3Presigner presigner;
+
+    @Mock
+    private PresignedGetObjectRequest presignedGetObjectRequest;
+
+    private PresignedUrlController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        controller = new PresignedUrlController(presigner);
+    }
+
+    @Test
+    void testGeneratePresignedUrlSuccess() throws Exception {
+        String bucket = "test-bucket";
+        String object = "test-object";
+        String expectedUrl = "https://s3.amazonaws.com/presigned-url";
+
+        when(presigner.presignGetObject(any(GetObjectPresignRequest.class)))
+                .thenReturn(presignedGetObjectRequest);
+
+        when(presignedGetObjectRequest.url())
+                .thenReturn(new URI(expectedUrl).toURL());
+
+        StepVerifier.create(controller.generatePresignedUrl(bucket, object))
+                .assertNext(response -> {
+                    assert response.getStatusCode().is2xxSuccessful();
+                    assert response.getBody().equals(expectedUrl);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void testGeneratePresignedUrlWithSpecialCharacters() throws Exception {
+        String bucket = "test-bucket";
+        String object = "folder/test-object.txt";
+        String expectedUrl = "https://s3.amazonaws.com/presigned-url";
+
+        when(presigner.presignGetObject(any(GetObjectPresignRequest.class)))
+                .thenReturn(presignedGetObjectRequest);
+
+        when(presignedGetObjectRequest.url()).thenReturn(new URI(expectedUrl).toURL());
+
+        StepVerifier.create(controller.generatePresignedUrl(bucket, object))
+                .assertNext(response -> {
+                    assert response.getStatusCode().is2xxSuccessful();
+                })
+                .verifyComplete();
+    }
+}
